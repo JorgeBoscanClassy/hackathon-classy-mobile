857514ec51a54918e5ac35f8a096186ed6fae2f9
Jorge Boscan <jboscan@classy.org>
2022-04-27 19:34:37 -0400

    Crossing fingers

--- classy-mobile-backend/oldsse/oldsse.go
+++ classy-mobile-backend/oldsse/oldsse.go
@@ -1,8 +1,9 @@
 package oldsse
 
 import (
-	"io"
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,8 +22,7 @@ func HandleSSEGin() gin.HandlerFunc {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		// instantiate the channel
-		var messageChan chan string
-		messageChan = make(chan string)
+		messageChan := make(chan string)
 		MessageList = append(MessageList, messageChan)
 
 		// close the channel after exit the function
@@ -32,6 +32,7 @@ func HandleSSEGin() gin.HandlerFunc {
 			log.Printf("client connection is closed")
 		}()
 
+		flusher, _ := w.(http.Flusher)
 		// trap the request under loop forever
 		for {
 
@@ -39,12 +40,9 @@ func HandleSSEGin() gin.HandlerFunc {
 
 			// message will received here and printed
 			case message := <-messageChan:
-
-				ctx.Stream(func(w io.Writer) bool {
-					// Stream message to client from message channel
-					ctx.SSEvent("message", message)
-					return true
-				})
+				fmt.Fprintf(w, "event: %s\n", "message")
+				fmt.Fprintf(w, "data: %s\n\n", message)
+				flusher.Flush()
 
 			// connection is closed then defer will be executed
 			case <-r.Context().Done():
@@ -57,6 +55,8 @@ func HandleSSEGin() gin.HandlerFunc {
 func SendMessageGin(message string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		for _, messageChan := range MessageList {
+			log.Printf("print message to client")
+
 			// send the message through the available channel
 			messageChan <- message
 		}
