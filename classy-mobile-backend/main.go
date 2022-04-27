6bc4212549b5657ad8f1b621217bc59bd82b7b9b
Patrick Coakley <pcoakley@classy.org>
2022-04-27 15:49:30 -0400

    add endpoints

--- classy-mobile-backend/main.go
+++ classy-mobile-backend/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+var donations map[string]Donation = make(map[string]Donation)
+var nextId int = 0
 
 func main() {
 	fmt.Println("Running Gin implementation on http://localhost:4000")
@@ -14,8 +19,39 @@ func main() {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
-	r.GET("/hello", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "World")
+	r.GET("/donations/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		if donation, ok := donations[id]; ok {
+			c.JSON(http.StatusOK, donation)
+			return
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{"error": "donation id does not exist"})
 	})
+
+	r.POST("/donations/", func(c *gin.Context) {
+		var donation Donation
+		if err := c.BindJSON(&donation); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		id := strconv.Itoa(nextId)
+		nextId += 1
+
+		donation.Id = id
+		donation.CreatedOn = time.Now()
+		donations[id] = donation
+		c.JSON(http.StatusCreated, donation)
+	})
+
 	r.Run(":4000")
+}
+
+type Donation struct {
+	Id           string    `json:"id"`
+	Amount       string    `json:"amount" binding:"required"`
+	Name         string    `json:"name" binding:"required"`
+	Email        string    `json:"email" binding:"required"`
+	Organization string    `json:"organization" binding:"required"`
+	CreatedOn    time.Time `json:"createdOn"`
 }
